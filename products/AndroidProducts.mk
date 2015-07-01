67984348c2d219706c3b7fd19137e7174eddf4b3
Matthew Alex <matthewalex@outlook.com>
2015-06-30 21:42:42 -0400

    Add our devices to androidproducts

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,2 +1,25 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/exsanguination_n6.mk
+# Copyright (C) 2015 Exsanguination Project
+#
+# Licensed under the Apurityche License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apurityche.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq (exsanguination_n5,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/exsanguination_n5.mk
+endif
+
+ifeq (exsanguination_n6,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/exsanguination_n6.mk
+endif
+
+ifeq (exsanguination_n4,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/exsanguination_n4.mk
+endif
